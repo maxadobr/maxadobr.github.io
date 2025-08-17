3d02ced4cc6d5c1a8134c8a3bdc531c461ecffcc
maxadobr <maxado.mdx@gmail.com>
2025-08-17 00:38:42 -0300

    feat: structured a scalable dark mode, finished positioning and added the remaining assets

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,31 +1,37 @@
 const toggleTheme = document.getElementById("toggleTheme");
-const rootHtml = document.documentElement
-const accordionHeaders = document.querySelectorAll(".accordion__header");
-const menuLinks = document.querySelectorAll(".menu__link");
+const rootHtml = document.documentElement;
+const sectionlink = document.querySelectorAll(".section-link");
+const themeIcons = document.querySelectorAll(".theme-icon");
 
-function changeTheme(){
+function updateIcons(theme) {
+    themeIcons.forEach(icon => {
+        const newSrc = icon.getAttribute(`data-src-${theme}`);
+        if (newSrc) {
+            icon.setAttribute('src', newSrc);
+        }
+    });
+}
+
+function changeTheme() {
     const currentTheme = rootHtml.getAttribute("data-theme");
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
 
-    currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark")
+    rootHtml.setAttribute("data-theme", newTheme);
+    updateIcons(newTheme);
+}
 
-    toggleTheme.classList.toggle("bi-sun")
-    toggleTheme.classList.toggle("bi-moon-stars")
+function initializeTheme() {
+    const currentTheme = rootHtml.getAttribute("data-theme");
+    updateIcons(currentTheme);
 }
 
 toggleTheme.addEventListener("click", changeTheme);
 
-accordionHeaders.forEach(header => {
-    header.addEventListener("click", () => {
-        const accordionItem = header.parentElement;
-        const accordionActive = accordionItem.classList.contains("active");
-
-        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
-    })
-})
-
-menuLinks.forEach(item => {
+sectionlink.forEach(item => {
     item.addEventListener("click", () => {
-        menuLinks.forEach(i => i.classList.remove("active"));
+        sectionlink.forEach(i => i.classList.remove("active"));
         item.classList.add("active");
-    })
-})
+    });
+});
+
+initializeTheme();
